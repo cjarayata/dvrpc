c54eba9740abc6b678e3cb117d8639b22e47b8e9
cjarayata <charles.arayata@gmail.com>
2022-12-09 22:01:33 -0500

    Dummy report rendering.

--- 2022_exercise/construct_report.R
+++ 2022_exercise/construct_report.R
@@ -2,13 +2,15 @@
 # basically, this file will source generate_data and, depending on the output, will either
 # render sample_report.Rmd or not
 
+# read in existing cache file from last run
+dataset_details <- read_parquet(here::here("2022_exercise", "cache_data_details.parquet"))
+
 
 library(quarto)
 
-source()
 
 if(good_to_go){
-  quarto_render("sample_report.Rmd", output_format = "word")
+  quarto_render(here::here("2022_exercise", "sample_report.Rmd"), output_format = "docx")
 }
 # save as temp file
 
